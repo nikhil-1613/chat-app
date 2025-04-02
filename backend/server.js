54cc42e160aa490ac730559d7ad91de1d2d21c6d
nikhil-1613 <nikhilgupta1613@gmail.com>
2025-04-02 12:06:42 +0530

    modified:   backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -23,12 +23,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 // CORS settings (Allows both local & deployed frontend)
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL || "http://localhost:3000",
+//     credentials: true,
+//   })
+// );
 
 // Routes
 app.use("/api/auth", authRoutes);
